80cb4bb39d42259b4ecd318f4855306dcc86794d
Kelvin <kelvinkipchumba935@gmail.com>
2026-04-01 10:49:30 +0300

    Update: changed to render the login view on opening

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-package com.system;
-
-import com.system.view.MainDashboard;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
 public class Main {
     public static void main(String[] args) {
-
-        // Tell Java to use the native Operating System theme
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            System.out.println("Could not load system theme.");
-        }
-
-        SwingUtilities.invokeLater(() -> {
-            MainDashboard dashboard = new MainDashboard();
-            dashboard.setVisible(true);
+        // Starts the application at login view
+        java.awt.EventQueue.invokeLater(() -> {
+            new com.system.view.LoginView().setVisible(true);
         });
     }
 }
